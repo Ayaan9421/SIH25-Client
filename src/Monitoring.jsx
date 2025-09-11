a623e9a947d9cac16be6aadac5970e45ac9e4050
samee147 <sameepshinde2005@gmail.com>
2025-09-11 23:44:09 +0530

    updated code added

---
+++ src/Monitoring.jsx
@@ -0,0 +1,5 @@
+export default function Monitoring({type}){
+    return(
+        <h2 style={{margin:"1rem"}}>{type}</h2>
+    );
+}
---
+++ src/Riskalerts.jsx
@@ -0,0 +1,7 @@
+export default function Riskalerts(){
+    return(
+        <div>
+            <h2 style={{margin:"1rem"}}>risks</h2>
+        </div>
+    )
+}
