a3b8875324ccbd72c2456ed1621968be3d509668
s-amin-05 <aminsolkar5@gmail.com>
2025-09-16 09:28:11 +0530

    Added weather data to weather data

--- src/Monitoring.jsx
+++ src/Monitoring.jsx
@@ -1,5 +1,10 @@
-export default function Monitoring({type}){
-    return(
-        <h2 style={{margin:"1rem"}}>{type}</h2>
-    );
+import Analytics from "./analyticsAndTools/Analytics.jsx";
+
+export default function Monitoring({ type }) {
+  return (
+    <>
+      <h2 style={{ margin: "1rem" }}>{type}</h2>
+      {type === "weather data" && <Analytics />}
+    </>
+  );
 }
